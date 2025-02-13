f8a11394296256efe0104d2d52c1f33ead419b74
zxzxcx <79790551+zxzxcx@users.noreply.github.com>
2025-02-13 13:54:56 +0900

    Update whileLab02.c

--- 250213/whileLab02.c
+++ 250213/whileLab02.c
@@ -5,7 +5,7 @@ void main()
 {
     int n_Value; //1부터 N까지 짝수만 출력하기 N
     printf("숫자 N을 정수로 적어주세요 : ");
-    scanf("%d",&n_Value);
+    scanf("%d", &n_Value);
     int i = 0;
     printf("숫자 1부터 N까지 짝수만 출력하겠습니다\n");
     while(i < n_Value)
