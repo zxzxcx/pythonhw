c3e3cea8f92e6520d3d280ff0d033fb0c93de89e
zxzxcx <79790551+zxzxcx@users.noreply.github.com>
2025-02-13 11:24:53 +0900

    Update 숫자피라미드.c

--- 250213/숫자피라미드.c
+++ 250213/숫자피라미드.c
@@ -9,10 +9,12 @@ void main()
     
     for ( int i = 0; i < n_Height + 1; i ++)
     {
+        //j만큼 공백을 발생하는 for문
         for (int j = n_Height - i; j > 0; j --)
         {
             printf(" ");
         }
+        //i번 숫자 출력 for문문
         for (int j = 0; j < i; j++)
         {
             printf("%d ", j + 1);
