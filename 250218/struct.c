d6445ef869638815a0daf5fa179160c294bf66b4
zxzxcx <79790551+zxzxcx@users.noreply.github.com>
2025-02-19 08:32:54 +0900

    Create struct.c

---
+++ 250218/struct.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main()
+{
+	struct student
+	{
+		char name[15];
+		int age;
+		char grade;
+	};
+	
+	struct student s1;
+	printf("Write your name : \n");
+	scanf("%s", s1.name);
+	printf("Write your age : \n");
+	scanf("%d", &s1.age);
+	
+	getchar();
+	printf("Write your grade : \n");
+	scanf("%c", &s1.grade);
+	
+	printf("%s\n", s1.name);
+	printf("%d\n", s1.age);
+  printf("%c\n", s1.grade);
+}
