06f5cb48f2b97261b99eb596721dbaf1efb00daa
zxzxcx <79790551+zxzxcx@users.noreply.github.com>
2025-02-20 08:41:44 +0900

    Update enum.c

--- 250218/enum.c
+++ 250218/enum.c
@@ -1,34 +1,49 @@
 #include <stdio.h>
+#include <string.h>
 
 void main()
 {
-	enum car
-	{
-		
-		SEDAN = 1,
-		SUV,
-		TRUCK,
-		VAN
-	};
-	
-	enum car c;
-	int car_int;
-	printf("1.SEDAN 2.SUV 3.TRUCK 4.VAN \n");
-	printf("Write car_type in integer : \n");
-	scanf("%d", &car_int);
-	if (car_int == 1 )
-	{
-		printf("SEDAN\n");
-		c = SEDAN;
-		printf("%d\n", SEDAN);
-	} else if (car_int == 2) {
-		printf("SUV\n");
-		c = SUV;
-	} else if (car_int == 3) {
-		printf("TRUCK\n");
-		c = TRUCK;
-	} else if (car_int == 4) {
-		printf("VAN\n");
-		c = VAN;
-	}	
+    enum car
+    {
+        SEDAN = 1,
+        SUV,
+        TRUCK,
+        VAN
+    };
+
+    enum car c;
+    char car_str[20];
+
+    printf("Write car type (SEDAN, SUV, TRUCK, VAN), (AVANTE, SANTAFE): \n");
+    scanf("%s", car_str);
+
+    // 문자열에 맞는 차종을 선택
+    if (strcmp(car_str, "SEDAN") == 0 || strcmp(car_str, "AVANTE") == 0)
+    {
+        printf("SEDAN\n");
+        c = SEDAN;
+        printf("Enum value: %d\n", c);
+    }
+    else if (strcmp(car_str, "SUV") == 0 || strcmp(car_str, "SANTAFE") == 0)
+    {
+        printf("SUV\n");
+        c = SUV;
+        printf("Enum value: %d\n", c);
+    }
+    else if (strcmp(car_str, "TRUCK") == 0)
+    {
+        printf("TRUCK\n");
+        c = TRUCK;
+        printf("Enum value: %d\n", c);
+    }
+    else if (strcmp(car_str, "VAN") == 0)
+    {
+        printf("VAN\n");
+        c = VAN;
+        printf("Enum value: %d\n", c);
+    }
+    else
+    {
+        printf("Error.\n");
+    }
 }
