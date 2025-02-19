a1d8624fb030131846deac294d112199bec89308
zxzxcx <79790551+zxzxcx@users.noreply.github.com>
2025-02-19 09:25:54 +0900

    Update union.c

--- 250218/union.c
+++ 250218/union.c
@@ -12,13 +12,17 @@ void main()
 	union student u;
 	printf("Write intval : \n");
 	scanf("%d", &u.intVal);
-	printf("value : %d addres : %p\n", u.intVal, &u.intVal);
+	printf("int value : %d addres : %p\n", u.intVal, &u.intVal);
 	printf("Write floatVal : \n");
 	scanf("%f", &u.floatVal);
-	printf("value : %f addres : %p\n", u.floatVal, &u.floatVal);
+	printf("int value : %d addres : %p\n", u.intVal, &u.intVal);
+	printf("float value : %f addres : %p\n", u.floatVal, &u.floatVal);
 	getchar();
 	printf("Write charVal : \n");
 	scanf("%c", &u.charVal);
-	printf("value : %c addres : %p\n", u.charVal, &u.charVal);
+	printf("int value : %d addres : %p\n", u.intVal, &u.intVal);
+	printf("float value : %f addres : %p\n", u.floatVal, &u.floatVal);
+	printf("char value : %c addres : %p\n", u.charVal, &u.charVal);
+
 	
 }
